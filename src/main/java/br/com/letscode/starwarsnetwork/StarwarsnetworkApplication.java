8a0921ac3741e719968ebbccfaec67f392879fe5
Vinicius Reis <vinicius.reis@msn.com>
2021-10-11 16:46:19 -0300

    init

---
+++ src/main/java/br/com/letscode/starwarsnetwork/StarwarsnetworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.starwarsnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/letscode/starwarsnetwork/StarwarsnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.starwarsnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
