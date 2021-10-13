1d7642b791b5ae834a93fa2bb827eeabcd553a5a
Vinicius Reis <vinicius.reis@msn.com>
2021-10-13 07:24:42 -0300

    implementa api atualiza localizacao rebelde

--- src/main/java/br/com/letscode/starwarsnetwork/rebelde/application/api/RebeldeAPI.java
+++ src/main/java/br/com/letscode/starwarsnetwork/rebelde/application/api/RebeldeAPI.java
@@ -1,8 +1,12 @@
 package br.com.letscode.starwarsnetwork.rebelde.application.api;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.letscode.starwarsnetwork.rebelde.application.api.request.RebeldeRequest;
+import br.com.letscode.starwarsnetwork.rebelde.domain.Localizacao;
 
 @RestController
 @RequestMapping(value = "/v1/rebelde")
@@ -17,4 +22,8 @@ public interface RebeldeAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	RebeldeDetalhadoResponse adicionaRebelde(@RequestBody @Valid RebeldeRequest rebeldeRequest);
+
+	@PatchMapping("/{idRebelde}/localizacao")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void atualizaLocalizacaoRebelde(@PathVariable UUID idRebelde, @RequestBody @Valid Localizacao localizacao);
 }
--- src/main/java/br/com/letscode/starwarsnetwork/rebelde/application/api/RebeldeController.java
+++ src/main/java/br/com/letscode/starwarsnetwork/rebelde/application/api/RebeldeController.java
@@ -1,11 +1,14 @@
 package br.com.letscode.starwarsnetwork.rebelde.application.api;
 
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.letscode.starwarsnetwork.rebelde.application.api.request.RebeldeRequest;
 import br.com.letscode.starwarsnetwork.rebelde.application.service.RebeldeService;
+import br.com.letscode.starwarsnetwork.rebelde.domain.Localizacao;
 import br.com.letscode.starwarsnetwork.rebelde.domain.Rebelde;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -23,5 +26,12 @@ public class RebeldeController implements RebeldeAPI {
 		Rebelde rebeldeAdicionado = rebeldeService.adicionaRebelde(rebeldeRequest.toDomain());
 		log.info("[finish] RebeldeController - adicionaRebelde");
 		return new RebeldeDetalhadoResponse(rebeldeAdicionado);
+	}
+
+	@Override
+	public void atualizaLocalizacaoRebelde(UUID idRebelde, @Valid Localizacao localizacao) {
+		log.info("[start] RebeldeController - atualizaLocalizacaoRebelde");
+		log.debug("localizacao = {}", localizacao);
+		log.info("[finish] RebeldeController - atualizaLocalizacaoRebelde");
 	}
 }
