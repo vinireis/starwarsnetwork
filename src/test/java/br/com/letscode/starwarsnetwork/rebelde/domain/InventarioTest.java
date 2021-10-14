47f5c95270543927f85be07e41ab79ccdd1b3606
Vinicius Reis <vinicius.reis@msn.com>
2021-10-14 14:54:32 -0300

    implementa test calcula pontos zerado

--- src/test/java/br/com/letscode/starwarsnetwork/rebelde/domain/InventarioTest.java
+++ src/test/java/br/com/letscode/starwarsnetwork/rebelde/domain/InventarioTest.java
@@ -1,7 +1,8 @@
 package br.com.letscode.starwarsnetwork.rebelde.domain;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class InventarioTest {
 
 	@Test
-	void dadoResistenciaPadrao_quandoTentarModificarBlackList_EntaoDeveSerLancadaUmExcecao() {
+	void dadoInventario_quandoTentarModificarEstoque_EntaoDeveSerLancadaUmExcecao() {
 		assertThrows(UnsupportedOperationException.class,
 				() -> buildInventario(Map.ofEntries(Map.entry(TipoItem.AGUA, 5)))
 				.getEstoque().put(TipoItem.COMIDA,10));
@@ -19,5 +20,12 @@ class InventarioTest {
 		return Inventario.builder()
 				.estoque(estoque)
 				.build();
+	}
+	
+	@Test
+	void dadoInventarioVazio_quandoCacularPontos_EntaoDeveRetornaZero() {
+		Inventario inventario = buildInventario(new HashMap<TipoItem, Integer>());
+		Integer pontos = Inventario.calculaPontos(inventario.getEstoque());
+		assertEquals(pontos, 0);
 	}
 }
